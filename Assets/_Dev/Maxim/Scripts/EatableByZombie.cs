7b4d1db764597ae945cc8a991efb20a7b203a83d
Maxim <maze.zte@gmail.com>
2024-08-20 13:12:17 +0900

    Add onDamage event to Eatable component

--- Assets/_Dev/Maxim/Scripts/EatableByZombie.cs
+++ Assets/_Dev/Maxim/Scripts/EatableByZombie.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EatableByZombie : MonoBehaviour
 {
     [SerializeField]
     private int health = 1;
 
+    [SerializeField]
+    private UnityEvent onDamage;
+
     public bool IsEated => health <= 0;
 
     public bool Eated()
     {
+        if (health > 0)
+        {
+            onDamage?.Invoke();
+        }
+
         health -= 1;
         if (health <= 0)
         {
